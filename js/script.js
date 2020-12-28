87f430a5993ed1d34ab19c0f66db525ef8274714
Manoel Blue <manoelbluee@gmail.com>
2020-12-28 08:00:41 +0100

    Create displayNineStudents function

--- js/script.js
+++ js/script.js
@@ -17,7 +17,29 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function displayNineStudents(studentsList, pageNum) {
+    let startIndex = (pageNum * 9) - 9;
+    let endIndex = pageNum * 9;
+    const studentsUL = document.querySelector('.student-list');
 
+    studentsUL.innerHTML = '';
+    for (let i = 0; i < studentsList.length; i++) {
+        if ( i >= startIndex && i < endIndex ) {
+            studentsUL.textContent += `
+                <li class="student-item cf">
+                    <div class="student-details">
+                    <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
+                    <h3>Ethel Dean</h3>
+                    <span class="email">ethel.dean@example.com</span>
+                    </div>
+                    <div class="joined-details">
+                    <span class="date">Joined 12-15-2005</span>
+                    </div>
+                </li>
+            `
+        }
+    };
+};
 
 
 /*
